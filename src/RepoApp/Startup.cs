7161a2ead3e2610e0332235c5caa518209fe5cba
Rinsen <fredrikh@outlook.com>
2015-08-13 09:16:03 +0200

    Not much left..

--- src/RepoApp/Startup.cs
+++ src/RepoApp/Startup.cs
@@ -1,25 +1,11 @@
-﻿using Microsoft.AspNet.Builder;
+﻿
+using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
-using Microsoft.AspNet.Hosting;
-using Microsoft.Framework.Runtime;
-using Microsoft.Framework.Configuration;
 
 namespace RepoApp
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; set; }
-
-        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
-        {
-            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
-                //.AddJsonFile("config.json")
-                .AddJsonFile($"config.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-
-            Configuration = builder.Build();
-        }
-
 
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -31,7 +17,7 @@ namespace RepoApp
         public void Configure(IApplicationBuilder app)
         {
             // Add static files to the request pipeline.
-            app.UseStaticFiles();
+            //app.UseStaticFiles();
 
             // Add MVC to the request pipeline.
             app.UseMvc(routes =>
@@ -40,9 +26,6 @@ namespace RepoApp
                     name: "default",
                     template: "{controller}/{action}/{id?}",
                     defaults: new { controller = "Home", action = "Index" });
-                routes.MapRoute(
-                    name: "apiAreaRoute",
-                    template: "api/{controller}/{action}");
             });
         }
     }
